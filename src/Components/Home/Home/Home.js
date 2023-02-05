8f3c32946bd3f7cd1a8a51aabb0e84675a8268b3
unknown <masummim50@gmail.com>
2023-02-05 20:42:10 +0600

    popular blog posts

--- src/Components/Home/Home/Home.js
+++ src/Components/Home/Home/Home.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import Banner from '../Banner/Banner';
+import PopularPostContainer from '../MostPopularBlog/PopularPostContainer';
 
 const Home = () => {
+
+
     return (
         <div>
             <h1>This is Home</h1>
 
             <Banner></Banner>
+            <PopularPostContainer/>
         </div>
     );
 };
